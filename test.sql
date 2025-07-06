0c3d917c320442003e57343d04a4596d81a83a6b
Bob <bobpitaia@gmail.com>
2025-07-06 22:27:45 +0000

    Conditional Operators

--- test.sql
+++ test.sql
@@ -1,3 +1,60 @@
+/*CONDICIONALES 
+AND, OR
+Y NOT
+*/
+
+select * from PELICULAS WHERE actor='Tom Cruise' or ACTOR='Richard Gere' or TITULO LIKE '%Potter%' and duracion < 150;
+
+
+--TABLA A CREAR
+create table peliculas(
+  codigo number(4) primary key,
+  titulo varchar2(40) not null,
+  actor varchar2(20),
+  duracion number(3)
+  );
+
+ --Ingreso de registros
+
+ insert into peliculas values(1020,'Mision imposible','Tom Cruise',120);
+ insert into peliculas values(1021,'Harry Potter y la piedra filosofal','Daniel R.',180);
+ insert into peliculas values(1022,'Harry Potter y la camara secreta','Daniel R.',190);
+ insert into peliculas values(1200,'Mision imposible 2','Tom Cruise',120);
+ insert into peliculas values(1234,'Mujer bonita','Richard Gere',120);
+ insert into peliculas values(900,'Tootsie','D. Hoffman',90);
+ insert into peliculas values(1300,'Un oso rojo','Julio Chavez',100);
+ insert into peliculas values(1301,'Elsa y Fred','China Zorrilla',110);
+
+
+
+--ORDER BY
+SELECT * from ARTICULOS ORDER BY ID_ARTICULO desc;
+
+SELECT * from ARTICULOS ORDER BY 2 asc;
+
+--TABLE TO CREATE
+CREATE TABLE ARTICULOS(
+ID_ARTICULO	INT NOT NULL PRIMARY KEY,
+ARTICULO VARCHAR2(50) NOT NULL,
+COD_FABRICANTE	NUMBER(10) NOT NULL,
+PESO NUMBER(4) NOT NULL,
+CATEGORIA	VARCHAR2(50),
+PRECIO_VENTA NUMBER(6,2),
+PRECIO_COSTO NUMBER(6,2),
+EXISTENCIAS	NUMBER(5)
+);
+     
+insert into articulos values(2,'TALADRO WALT',1012546955,45,'CONSTRUCCION',400,560,20);
+insert into articulos values(3,'ALICATE ACE',1012432365,23,'MECANICA',200,350.25,120);
+insert into articulos values(4,'PALA SENIC',1012345678,16,'CONSTRUCCION',100,230.5,200);
+insert into articulos values(5,'TALADRO MILLWAKEE',1012545210,55,'CONSTRUCCION',300,423.5,50);
+insert into articulos values(6,'GRIFO APERTURA MANOS',1010023654,26,'PLOMERIA',400,560.25,90);
+insert into articulos values(7,'FREGADERO MURAL',1015487966,200,'PLOMERIA',600,780.50,52);
+insert into articulos values(8,'TALADRO ELECTRICO BOSCH',1025493013,34,'CONSTRUCCION',250,380.25,41);
+insert into articulos values(9,'CEPILLO CONICO ONDULADO',1025852000,44,'EBANISTERIA',150,240.25,88);
+insert into articulos values(10,'CEPILLO TAZA ONDULADO',122247789,49,'EBANISTERIA',148,240.50,90);
+insert into articulos values(11,'CEPILLO TAZA TRENZADA',1016467985,	42,'EBANISTERIA',139,210.50,89);
+
 /*FUNCIONES 
 DATE
 */
