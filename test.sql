20a699e998674830c1ae4ca99266429102c0918b
Bob <bobpitaia@gmail.com>
2025-07-07 16:02:34 +0000

    Between

--- test.sql
+++ test.sql
@@ -1,3 +1,37 @@
+/*
+BETWEEN
+*/
+
+select * from MEDICAMENTOS where precio between 5 and 15 order by CANTIDAD desc;
+
+select * from MEDICAMENTOS where cantidad BETWEEN 100 and 200 order by CODIGO;
+
+select * from MEDICAMENTOS where FECHAVENCIMIENTO BETWEEN TO_DATE('01/12/2017', 'DD/MM/YYYY') and TO_DATE('01/02/2018', 'DD/MM/YYYY');
+drop table medicamentos;
+
+ create table medicamentos(
+  codigo number(6) not null,
+  nombre varchar2(20),
+  laboratorio varchar2(20),
+  precio number(6,2),
+  cantidad number(4),
+  fechavencimiento date not null,
+  primary key(codigo)
+ );
+
+ insert into medicamentos
+  values(102,'Sertal','Roche',5.2,10,to_date('01/02/2020','dd/mm/yyyy'));
+ insert into medicamentos 
+  values(120,'Buscapina','Roche',4.10,200,to_date('01/12/2017','dd/mm/yyyy'));
+ insert into medicamentos 
+  values(230,'Amoxidal 500','Bayer',15.60,100,to_date('28/12/2017','dd/mm/yyyy'));
+ insert into medicamentos
+  values(250,'Paracetamol 500','Bago',1.90,20,to_date('01/02/2018','dd/mm/yyyy'));
+ insert into medicamentos 
+  values(350,'Bayaspirina','Bayer',2.10,150,to_date('01/12/2019','dd/mm/yyyy'));
+ insert into medicamentos 
+  values(456,'Amoxidal jarabe','Bayer',5.10,250,to_date('01/10/2020','dd/mm/yyyy'));
+
 /*CONDICIONALES 
 AND, OR
 Y NOT
