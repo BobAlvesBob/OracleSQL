5e9b0297a3dd2908326ac8a44d98527a45b14dd8
Bob <bobpitaia@gmail.com>
2025-07-07 17:09:02 +0000

    LIKE, NOT LIKE

--- test.sql
+++ test.sql
@@ -1,4 +1,54 @@
 /*
+LIKE, 
+NOT LIKE
+*/
+
+SELECT * FROM EMPLEADOS WHERE DOMICILIO LIKE '%Colon%';
+
+SELECT * FROM EMPLEADOS WHERE nombre LIKE '%erez%';
+
+select * from empleados where nombre not like '%erez%';
+
+select * from empleados where documento like '35%';--35 AL PRINCIPIO
+
+select * from empleados where documento like '35%' and DOMICILIO not like '%Col%';
+
+select * from empleados where documento like '35%8%';
+
+select * from empleados where seccion like 'G_______';
+
+select * from empleados where sueldo like '5__%';--DECIMALES SON IGNORADOS
+
+
+drop table empleados;
+ 
+ create table empleados(
+  nombre varchar2(30),
+  documento char(8) not null,
+  domicilio varchar2(30),
+  fechaingreso date,
+  seccion varchar2(20),
+  sueldo number(6,2),
+  primary key(documento)
+ );
+  insert into empleados
+  values('Juan Perez','22333444','Colon 123',to_date('08/10/1990','dd/mm/yyyy'),'Gerencia',900.50);
+ insert into empleados
+  values('Ana Acosta','23444555','Caseros 987',to_date('18/12/1995','dd/mm/yyyy'),'Secretaria',590.30);
+ insert into empleados
+  values('Lucas Duarte','25666777','Sucre 235',to_date('15/05/2005','dd/mm/yyyy'),'Sistemas',790);
+ insert into empleados
+  values('Pamela Gonzalez','26777888','Sarmiento 873',to_date('12/02/1999','dd/mm/yyyy'),'Secretaria',550);
+ insert into empleados
+  values('Marcos Juarez','30000111','Rivadavia 801',to_date('22/09/2002','dd/mm/yyyy'),'Contaduria',630.70);
+ insert into empleados
+  values('Yolanda perez','35111222','Colon 180',to_date('08/10/1990','dd/mm/yyyy'),'Administracion',400);
+ insert into empleados
+  values('Rodolfo perez','35555888','Coronel Olmedo 588',to_date('28/05/1990','dd/mm/yyyy'),'Sistemas',800);
+
+
+
+/*
 IN, NOT IN
 */
 SELECT * FROM medicamentos WHERE LABORATORIO IN ('Bayer','Bago') ORDER BY 3 ASC;
