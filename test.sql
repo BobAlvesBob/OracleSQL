dee775048359ca60dce1bc1baa461dbfbcf5e8d4
Bob <bobpitaia@gmail.com>
2025-07-09 16:44:02 +0000

    FULL JOIN

--- test.sql
+++ test.sql
@@ -1,4 +1,60 @@
 /*
+FULL
+JOIN
+*/
+SELECT * from inscriptos;
+select * from deportes;
+
+select i.documento, i.matricula, de.nombre as Deporte, de.profesor
+from inscriptos i
+full join deportes de
+on de.codigo = i.CODIGODEPORTE;
+
+select d.nombre as "Nombre del Esporte", 
+d.profesor, 
+count(i.documento) as "Cantidad de Inscriptos" 
+ from deportes d 
+full join inscriptos i on d.CODIGO = i.CODIGODEPORTE
+group by d.nombre, d.profesor;
+
+drop table deportes;
+ drop table inscriptos;
+
+ create table deportes(
+  codigo number(2),
+  nombre varchar2(30),
+  profesor varchar2(30)
+ );
+
+ create table inscriptos(
+  documento char(8),
+  codigodeporte number(2),
+  matricula char(1) --'s'=paga; 'n'=impaga
+ );
+
+ alter table deportes
+  add constraint PK_deportes
+  primary key(codigo);
+
+ alter table inscriptos
+  add constraint PK_inscriptos
+  primary key(documento,codigodeporte);
+
+
+ insert into deportes values(1,'tenis','Marcelo Roca');
+ insert into deportes values(2,'natacion','Marta Torres');
+ insert into deportes values(3,'basquet','Luis Garcia');
+ insert into deportes values(4,'futbol','Marcelo Roca');
+ 
+ insert into inscriptos values('ins-0001',3,'s');
+ insert into inscriptos values('ins-0002',3,'s');
+ insert into inscriptos values('ins-0003',3,'n');
+ insert into inscriptos values('ins-0004',2,'s');
+ insert into inscriptos values('ins-0005',2,'s');
+ insert into inscriptos values('ins-0006',4,'n'); 
+ insert into inscriptos values('ins-0007',5,'n');
+
+/*
 RIGHT JOIN
 */
 
