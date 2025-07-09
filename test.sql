e3d6471fc33b844571f3d35a7224fb9444844f9f
Bob <bobpitaia@gmail.com>
2025-07-09 19:30:58 +0000

    SELF REFERENCED FK

--- test.sql
+++ test.sql
@@ -1,4 +1,33 @@
 /*
+ SELF -REFERENCED FK
+*/
+drop table clientes
+create table clientes(
+  codigo number(5), 
+  nombre varchar2(30), 
+  domicilio varchar2(30),
+  ciudad varchar2(20),
+  referenciadopor number(5),
+  primary key(codigo)
+);
+
+alter table clientes
+rename constraint SYS_C008264 to "pk_clientes";
+
+alter table clientes 
+add constraint fk_referencia
+foreign key (referenciadopor) 
+references clientes(codigo);
+
+update clientes set REFERENCIADOPOR = 90 where REFERENCIADOPOR = 9;
+
+
+ insert into clientes values (50,'Juan Perez','Sucre 123','Cordoba',null);
+ insert into clientes values(90,'Marta Juarez','Colon 345','Carlos Paz',null);
+ insert into clientes values(110,'Fabian Torres','San Martin 987','Cordoba',50);
+ insert into clientes values(125,'Susana Garcia','Colon 122','Carlos Paz',90);
+ insert into clientes values(140,'Ana Herrero','Colon 890','Carlos Paz',9);
+/*
 FOREIGN KEY
 */
 alter table clientes
