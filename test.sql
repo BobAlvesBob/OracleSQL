80f2c58e7503f8dcd464e7bde888e3f3609eb7c7
Bob <bobpitaia@gmail.com>
2025-07-09 19:58:19 +0000

    FK VALIDATE

--- test.sql
+++ test.sql
@@ -1,7 +1,67 @@
 /*
+FK VALIDATE
+*/
+insert into CLIENTES
+values(110, 'Garcia Omar', 'San Martin 100', 'La Pampa', 6);
+
+alter table clientes
+disable novalidate
+constraint fk_codigoprovincia;
+
+alter table clientes
+disable novalidate
+constraint fk_codigoprovincia;
+
+
+select constraint_name, status, validated
+from user_constraints
+where table_name = 'CLIENTES';
+
+alter table clientes
+add constraint fk_codigoprovincia
+foreign key(codigoprovincia) 
+references provincias(codigo)novalidate;
+
+SELECT * from clientes;
+
+select * from provincias;
+
+
+drop table clientes;
+drop table provincias;
+
+create table clientes (
+codigo number(5),
+nombre varchar2(30),
+domicilio varchar2(30),
+ciudad varchar2(20),
+codigoprovincia number(2),
+primary key(codigo)
+);
+
+create table provincias(
+codigo number(2),
+nombre varchar2(20),
+primary key (codigo)
+);
+
+insert into provincias values(1,'Cordoba');
+insert into provincias values(2,'Santa Fe');
+insert into provincias values(3,'Misiones');
+insert into provincias values(4,'Rio Negro');
+
+insert into clientes values(100,'Perez Juan','San Martin 123','Carlos Paz',1);
+insert into clientes values(101,'Moreno Marcos','Colon 234','Rosario',2);
+insert into clientes values(102,'Garcia Juan','Sucre 345','Cordoba',1);
+insert into clientes values(103,'Lopez Susana','Caseros 998','Posadas',3);
+insert into clientes values(104,'Marcelo Moreno','Peru 876','Viedma',4);
+insert into clientes values(105,'Lopez Sergio','Avellaneda 333','La Plata',5);
+
+/*
  SELF -REFERENCED FK
 */
-drop table clientes
+drop table clientes;
+
 create table clientes(
   codigo number(5), 
   nombre varchar2(30), 
