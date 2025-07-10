d58397e418a0a29a72559b3f30ee4db5f828b4f8
Bob <bobpitaia@gmail.com>
2025-07-10 00:14:39 +0000

    CREATE TABLE AS

--- test.sql
+++ test.sql
@@ -1,3 +1,42 @@
+/*
+CREATE TABLE AS
+*/
+
+CREATE TABLE PEDIDOS AS
+SELECT descripcion as Producto, precio as Monto
+from productos;
+
+select * from pedidos;
+SELECT * FROM PRODUCTOS;
+
+create table inventario as
+select descripcion as Articulo, existencia as Cantidad_tornillos
+from productos where descripcion = 'tornillos';
+
+select * from inventario;
+drop table productos;
+
+create table productos(
+idprod varchar2(8) not null primary key,
+descripcion varchar2(30) not null,
+precio number(6,2) default 0,
+existencia int default 0
+);
+insert into productos values('prd0001','arandela',580,200);
+insert into productos values('prd0002','bisagra',80.23,160);
+insert into productos values('prd0003','extractor',112.5,140);
+insert into productos values('prd0004','manivela',26.55,250);
+insert into productos values('prd0005','rodamiento',2888,30);
+insert into productos values('prd0006','plato', 265.35, 20);
+insert into productos values('prd0007','cubo',652.3,10);
+insert into productos values('prd0008','reostato',225.5,115);
+insert into productos values('prd0009','perno', 148,148);
+insert into productos values('prd0010','tornillos',243, 523);
+insert into productos values('prd0011','tornillos',1245.5, 645);
+insert into productos values('prd0012','tornillos',134.55, 900);
+insert into productos values('prd0013','bomba',350.5,578);
+insert into productos values('prd0014','junta',4500,489);
+
  /*
  SUBQUERIES
  IN, ANY, SOME
